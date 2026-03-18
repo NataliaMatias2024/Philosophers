cdaae4e47fed2ed55b36dae86aa70e13198e9c56
namatias <namatias@student.42sp.org.br>
2026-03-18 14:18:43 -0300

    Inicialize project structure

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,11 +6,17 @@
 /*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 14:14:19 by namatias          #+#    #+#             */
-/*   Updated: 2026/03/18 14:15:03 by namatias         ###   ########.fr       */
+/*   Updated: 2026/03/18 14:18:25 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
 
 #endif
